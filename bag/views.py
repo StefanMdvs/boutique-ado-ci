e85d5ef565b32202aa5b27590a5dc575b82d523a
StefanMdvs <stefan.medves@gmail.com>
2021-11-28 20:59:51 +0000

    Finish product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,15 +12,27 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # get the bag var if it exists in the session or create one if not
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        # update quantity if bag already exists
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        # add item to the bag
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            # update quantity if bag already exists
+            bag[item_id] += quantity
+        else:
+            # add item to the bag
+            bag[item_id] = quantity
     
     # finally overwrite the var in the session with the updated version
     request.session['bag'] = bag
